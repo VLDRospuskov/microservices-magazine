908ce8d813b7177d7454b34405a2bdf8a44b0a6b
Vladislav Rospuskov <Vladislav_Rospuskov@epam.com>
2019-06-04 17:05:15 +0300

    added product layer

---
+++ catalog-service/src/main/java/com.vldrospuskov.catalogservice/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.vldrospuskov.catalogservice.service;
+
+import com.vldrospuskov.catalogservice.entity.Product;
+import com.vldrospuskov.catalogservice.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findProductByCode(String code) {
+        return productRepository.findByCode(code);
+    }
+
+}
