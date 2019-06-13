460af450b931623365f52af1cd6bf6c3b2a6e775
Vladislav Rospuskov <Vladislav_Rospuskov@epam.com>
2019-06-14 01:57:36 +0300

    testing vualt

---
+++ config/vault-init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+VAULT_DEV_TOKEN=934f9eae-31ff-a8ef-e1ca-4bea9e07aa09
+
+vault login ${VAULT_DEV_TOKEN}
+
+vault secrets disable secret
+vault secrets enable -version=1 -path=secret kv
+vault kv put secret/catalog-service @${CONFIG_DIR}/catalog-service.json
